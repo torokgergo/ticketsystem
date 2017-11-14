8b52b773c29dc094fe98271a104fb3be12f42183
Petra <petrabedo@gmail.com>
2017-11-14 22:56:17 +0100

    Modified navbar

--- views/page/view.php
+++ views/page/view.php
@@ -12,28 +12,20 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="page-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="title1"><h1><?= Html::encode($this->title) ?></h1></div>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'title',
-            'content:ntext',
-            'create_date',
-            'user_id',
-        ],
-    ]) ?>
+
+
+<?php//= DetailView::widget([
+//        'model' => $model,
+//        'attributes' => [
+//            'id',
+//            'title',
+//            'content:ntext',
+//            'create_date',
+//            'user_id',
+//        ],
+//    ]) ?>
 
 </div>
