7d230025772db69499064bd28bae8dce64799273
Petra <petrabedo@gmail.com>
2017-11-14 14:35:59 +0100

    Frontpage ready

--- views/site/index.php
+++ views/site/index.php
@@ -1,52 +1,142 @@
-<?php
+<div class="jumbotron text-center">
 
-/* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
-?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Brafo, Brafo, Kecifei!<br>Dabzs, dabzs!</h1>
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+    <div class="row" id="box-search">
+        <div class="thumbnail text-center">
+            <img src="http://kephost.com/images/2017/11/07/sadasd.png" alt="" class="img-responsive">
+            <div class="caption">
+                <span style="text-shadow: #bababa 0px 3px 2px 1px; color: #ffffff; font-family:times new roman,times,serif;"><em><span style="font-size:48px;">Nagyon motiváló szöveg</span></em></span>
             </div>
         </div>
-
     </div>
+
+
+
+
+
+</div>
+
+<div class="container">
+    <div class="row">
+
+        <a href="link.php">
+            <div class="col-sm-4">
+                <div class="hovereffect">
+                    <img class="img-responsive img-rounded" src="http://kephost.com/images/2017/11/06/2ba46430acb308e95.png" width="74%" height="75%">
+                    <div class="overlay">
+
+                    </div>
+                </div>
+                <div class="text-center" style="text-transform: uppercase; background-color: #ecebeb; padding-top: 20px;"><h4><strong>Egyágyas szoba (2 személy)</strong></h4></div>
+                <div class="text-center"><button type="button"  style="text-transform: uppercase; letter-spacing: 2px; color: #5e5e5e; border: 0px;"><h4><strong>5999 Ft/fő/éj</strong></h4></button></div>
+            </div>
+        </a>
+
+        <a href="link.php">
+            <div class="col-sm-4">
+                <div class="hovereffect">
+                    <img class="img-responsive img-rounded" src="http://kepkezelo.com/images/9kp1f7blur6ag3x8rf1i.png" width="74%" height="75%">
+                    <div class="overlay">
+
+                    </div>
+                </div>
+                <div class="text-center" style="text-transform: uppercase; background-color: #ecebeb; padding-top: 20px;"><h4><strong>Kétágyas szoba (2 személy)</strong></h4></div>
+                <div class="text-center"><button type="button"  style="text-transform: uppercase; letter-spacing: 2px; color: #5e5e5e; border: 0px;"><h4><strong>6999 Ft/fő/éj</strong></h4></button></div>
+            </div>
+        </a>
+        <a href="link.php">
+            <div class="col-sm-4">
+                <div class="hovereffect">
+                    <img class="img-responsive img-rounded" src="http://kepkezelo.com/images/rtia6c2ctbh6plq0pp3o.png" width="74%" height="75%">
+                    <div class="overlay">
+
+                    </div>
+                </div>
+                <p>
+
+                <div class="text-center" style="text-transform: uppercase; background-color: #ecebeb; padding-top: 20px;"><h4><strong>Lakosztály (5 személy)</strong></h4></div>
+                <div class="text-center"><button type="button"  style="text-transform: uppercase; letter-spacing: 2px; color: #5e5e5e; border: 0px;"><h4><strong>9999 Ft/fő/éj</strong></h4></button></div>
+            </div>
+        </a>
+        <p></p>
+
+
+
+        <img src="http://kephost.com/images/2017/11/07/Untitled-8.png" alt="Untitled-8.png" border="0" height="100px"/>
+
+
+
+        <div class="row">
+            <div class="col-lg-5">
+                <img style="float:left" src="http://kephost.com/images/2017/11/07/logg.png"/>
+            </div>
+
+            <div class="col-lg-6">
+
+                <h3><em>Miért érdemes minket választani? </em></h3> <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li>
+                    Proin a eros convallis, vehicula velit eget, congue justo.</li>
+                <li>
+                    Nam non justo euismod, vulputate neque vitae, tempus dui.</li>
+                <li>
+                    Donec quis sapien in nulla euismod cursus.</li>
+                <li>
+                    Vestibulum gravida quam ut sollicitudin luctus.</li>
+                <li>
+                    Nam malesuada velit eu commodo consectetur.</li>
+                <li>
+                    Donec quis sapien in nulla euismod cursus.</li>
+                <li>
+                    Vestibulum gravida quam ut sollicitudin luctus.</li>
+                <li>
+                    Nam malesuada velit eu commodo consectetur.</li>
+
+
+            </div>
+
+
+            </p>
+        </div>
+        <img src="http://kephost.com/images/2017/11/07/Untitled-8.png" alt="Untitled-8.png" border="0" height="100px"/>
+        <h1 class="display-3" style="color: #009899">Rólunk mondták</h1>
+
+
+        <div class="weight: 60%">
+
+            <blockquote class="blockquote">
+                <p class="mb-0">When you choose the paradigm of service, it turns everything you do from a job into a gift.</p>
+                <p style="text-align: right; text-transform: uppercase; letter-spacing: 4px; font-size: 13px; background-color: #cbf0f0; font-color: #389d9e; padding: 10px;">
+                    Kovács Béla</p>
+
+            </blockquote>
+        </div>
+
+        <blockquote class="blockquote">
+            <p class="mb-0">If you have been touched by the success fairy, people think you know why. People think success breeds enlightenment and you are duty-bound to spread around like manure. Fertilize those young minds!</p>
+            <p style="text-align: right; text-transform: uppercase; letter-spacing: 4px; font-size: 13px; background-color: #cbf0f0; font-color: #389d9e; padding: 10px;">
+                Kovács Béla</p>
+        </blockquote>
+
+        <blockquote class="blockquote">
+            <p class="mb-0">A stupid man's report of what a clever man says can never be accurate, because he unconsciously translates what he hears into something he can understand.</p>
+            <p style="text-align: right; text-transform: uppercase; letter-spacing: 4px; font-size: 13px; background-color: #cbf0f0; font-color: #389d9e; padding: 10px;">
+                Kovács Béla</p>
+        </blockquote>
+
+        <blockquote class="blockquote">
+            <p class="mb-0">History teaches us that men and nations behave wisely once they have exhausted all other alternatives.</p>
+            <p style="text-align: right; text-transform: uppercase; letter-spacing: 4px; font-size: 13px; background-color: #cbf0f0; font-color: #389d9e; padding: 10px;">
+                Kovács Béla</p>
+        </blockquote>
+    </div>
+
+</div>
+
+
+
+
+
+
+</div>
+</div>
 </div>
