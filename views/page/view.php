8476a35d7a9b6a96471bee389a4b8971330a1e00
Petra <petrabedo@gmail.com>
2017-11-14 23:33:49 +0100

    Modified css

--- views/page/view.php
+++ views/page/view.php
@@ -14,9 +14,19 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="title1"><h1><?= Html::encode($this->title) ?></h1></div>
 
+
+
+
     <div class="content"><?= Html::encode($model->content) ?></div>
 
+    <p></p>
+    <div class="c">Creator:</div>
+    <div class="row">
+      <div class="col-sm-4">
 
+          <div class="username"> <?= $model->user->name ?></div>
+      </div>
 
+    <div class="regdate"> <?= $model->user->reg_date ?></div></div>
 
 </div>
